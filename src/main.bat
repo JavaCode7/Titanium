34e42db6dba89c51c3cb68a1481f8ed0737345cf
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-15 17:00:58 +0000

    Read from files

--- src/main.bat
+++ src/main.bat
@@ -1,9 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Read the file line by line
+:: Read the files the program needs line by line
 set count=0
-for /f "delims=*" %%a in (hello.tix) do (
+for /f "delims=*" %%a in (settings\keywords.txt) do (
    set /a count+=1
-   set lines[!count!] = %%a
+   set keywords[!count!] = %%a
+)
+
+for /f "delims=*" %%b in (%1) do (
+   set /a count+=1
+   set lines[!count!] = %%b
+   echo %%b
 )

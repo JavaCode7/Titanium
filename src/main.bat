f6df6ea2a98adb36003452acf1eb09365accf42f
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-15 16:41:23 +0000

    Update main.bat

--- src/main.bat
+++ src/main.bat
@@ -1,1 +1,9 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Read the file line by line
+set count=0
+for /f "delims=*" %%a in (hello.tix) do (
+   set /a count+=1
+   set lines[!count!] = %%a
+)

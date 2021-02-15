22d1d562aad569a851a13628060bef277e0f4cf0
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-15 21:47:09 +0000

    Added glider command :)

--- src/main.bat
+++ src/main.bat
@@ -1,15 +1,29 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo @echo off>%1.bat
+
 :: Read the files the program needs line by line
 set count=0
-for /f "delims=*" %%a in (settings\keywords.txt) do (
+for /f "delims=*" %%x in (settings\keywords.txt) do (
    set /a count+=1
-   set keywords[!count!] = %%a
+   set keywords[!count!]=%%x
 )
 
-for /f "delims=*" %%b in (%1) do (
+set count=0
+for /f "delims=*" %%y in (%1) do (
    set /a count+=1
-   set lines[!count!] = %%b
-   echo %%b
+   set lines[!count!]=%%y
+)
+
+:: Split each line into 2 sections:
+::    The command e.g. "glider:"
+::    The value e.g. "'Hello world'"
+:: And then compile to batch.
+for /F "delims=: tokens=1,2" %%a in ("glider: 'Hello World'") do (
+   echo `%%a`
+   echo `%%b`
+   if %%a == glider (
+      echo echo%%b>>%1.bat
+   )
 )
